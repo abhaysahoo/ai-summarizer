bf8df32b9f630064399b528a12a1b2151189a720
Abhay Sahoo <56788650+abhaysahoo@users.noreply.github.com>
2024-02-22 12:58:15 +0530

    changing 'GitHub' button url

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
 
             <button
                 type='button'
-                onClick={ () => window.open('https://github.com/abhaysahoo') }
+                onClick={ () => window.open('https://github.com/abhaysahoo/ai-summarizer') }
                 className='black_btn'
             >
                 GitHub
